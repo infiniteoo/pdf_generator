584f4fe4ebb01f07a3ea04b73cb26d1b9110da2f
Troy Dorman <troydorman@gmail.com>
2021-04-22 12:05:25 -0600

    set up inputs in client and implemented the handleChange method for them

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axois from "axios";
+import { saveAs } from "file-saver";
+
+class App extends React.Component {
+  state = {
+    name: '',
+    receiptId: 0,
+    price1: 0,
+    price2: 0,
+  };
+
+  handleChange = ({target: { value, name }}) => this.setState({[name]: value});
+
+  createAndDownloadPdf =
+   
+  
+ 
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          onChange={this.handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Receipt ID"
+          name="receiptId"
+          onChange={this.handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Price 1"
+          name="price1"
+          onChange={this.handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Price 2"
+          name="price2"
+          onChange={this.handleChange}
+        />
+        <button onClick={this.createAndDownloadPdf}>Download PDF.</button>
+      </div>
+    );
+  }
 }
 
 export default App;
