681aceb1f5bf406ce1a1b55346984a19c31eb953
Troy Dorman <troydorman@gmail.com>
2021-04-22 12:18:49 -0600

    created post route to receive state from client and use html-pdf to convert that data to a pdf

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const pdf = require('html-pdf');
 const cors = require('cors');
+const pdfTemplate = require('./documents');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,5 +12,29 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// POST ROUTE - PDF GENERATION AND FETCHING OF THE DATA
+app.post('/create-pdf', (req,res) => {
+
+    pdf.create(pdfTemplate(req.data), {}).toFile('result.pdf', (err) => {
+        if(err){
+            return Promise.reject();
+
+        }
+        return Promise.resolve();
+
+        
+    });
+
+
+
+})
+
+// GET ROUTE - SEND THE GENERATED PDF TO THE CLIENT
+
+
+
+
+
 app.listen(port, () => console.log(`listening on port ${port}`));
+
 
