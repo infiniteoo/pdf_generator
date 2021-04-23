4673a9c2b50d729ed074716384476014f28cc407
Troy Dorman <troydorman@gmail.com>
2021-04-22 21:39:46 -0600

    adding material ui components

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,94 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
+import { PhotoCamera } from "@material-ui/icons";
 
-import axois from "axios";
+import axios from "axios";
 import { saveAs } from "file-saver";
-import axios from 'axios';
+
+import {
+  Typography,
+  AppBar,
+  Card,
+  TextField,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  Button,
+} from "@material-ui/core";
 
 class App extends React.Component {
   state = {
-    name: '',
+    name: "",
     receiptId: 0,
     price1: 0,
     price2: 0,
   };
 
-  handleChange = ({target: { value, name }}) => this.setState({[name]: value});
+  handleChange = ({ target: { value, name } }) =>
+    this.setState({ [name]: value });
 
   createAndDownloadPdf = () => {
-    axios.post('/create-pdf', this.state)
-    .then(() => axios.get('/fetch-pdf', { responseType: 'blob' }))
-    .then((res) => {
-      const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
+    axios
+      .post("/create-pdf", this.state)
+      .then(() => axios.get("/fetch-pdf", { responseType: "blob" }))
+      .then((res) => {
+        const pdfBlob = new Blob([res.data], { type: "application/pdf" });
 
-      saveAs(pdfBlob, 'newPdf.pdf');
-    })
+        saveAs(pdfBlob, "newPdf.pdf");
+      });
+  };
 
-  }
-   
-  
- 
   render() {
     return (
-      <div className="App">
-        <input
-          type="text"
-          placeholder="Name"
-          name="name"
-          onChange={this.handleChange}
-        />
-        <input
-          type="number"
-          placeholder="Receipt ID"
-          name="receiptId"
-          onChange={this.handleChange}
-        />
-        <input
-          type="number"
-          placeholder="Price 1"
-          name="price1"
-          onChange={this.handleChange}
-        />
-        <input
-          type="number"
-          placeholder="Price 2"
-          name="price2"
-          onChange={this.handleChange}
-        />
-        <button onClick={this.createAndDownloadPdf}>Download PDF.</button>
-      </div>
+      <>
+        <CssBaseline />
+        <AppBar position="relative">
+          <Typography variant="h6">PDF Generator w/ React</Typography>
+        </AppBar>
+        <div className="App">
+        <Container maxWidth="sm">
+        <form /* className={classes.root} */ noValidate autoComplete="off">
+         
+          <TextField name="name" label="Name" variant="outlined" onChange={this.handleChange}/>
+          <TextField name="receiptId" label="Receipt ID" variant="outlined" onChange={this.handleChange}/>
+          <TextField name="price1" label="Price 1" variant="outlined" onChange={this.handleChange}/>
+          <TextField id="price2" label="Price 2" variant="outlined" onChange={this.handleChange}/>
+          <Button onClick={this.createAndDownloadPdf}>Download PDF </Button>
+        </form>
+        </Container>
+
+        
+          {/* <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            onChange={this.handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Receipt ID"
+            name="receiptId"
+            onChange={this.handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Price 1"
+            name="price1"
+            onChange={this.handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Price 2"
+            name="price2"
+            onChange={this.handleChange}
+          /> */}
+          
+        </div>
+      </>
     );
   }
 }
